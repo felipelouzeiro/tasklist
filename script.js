fc12a069d8ecccfb6104069cd019ba85bbea20c5
felipe <fl.louzeiro@gmail.com>
2022-05-24 12:05:03 -0300

    feat: implementa função para deletar um card

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 function addCard(element) {
-  const ul = element.previousElementSibling.id;
+  const ul = element.previousElementSibling.id; // pega o id do elemento irmão anterior
   console.log('ul: ', ul)
-  
+
   const task = prompt("Digite o nome da tarefa:");
   console.log('task: ', task)
-  
+
   const board = document.getElementById(ul);
   console.log('board: ', board)
 
-  const template = 
-    `<li>
+  const template =
+    `<li id="${new Date().getTime()}">
       <p>${task}</p>
-      <p class="remove">x</p>
+      <p class="remove" onClick="removeCard(this)">x</p>
     </li>`;
 
   board.innerHTML = board.innerHTML + template;
+}
+
+function removeCard(element) {
+  const li = element.parentElement.id; // pega o id do elemento pai
+  console.log('li: ', li)
+
+document.getElementById(li).remove();
 }
