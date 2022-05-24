b9df7c5b6405bf89c01b36fc4df66c7b7dc9a75a
felipe <fl.louzeiro@gmail.com>
2022-05-24 12:41:56 -0300

    feat: implementa drag e drop para mover um card

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function addCard(element) {
   console.log('board: ', board)
 
   const template =
-    `<li id="${new Date().getTime()}">
+    `<li id="${new Date().getTime()}" draggable="true" ondragstart="drag(event)">
       <p>${task}</p>
       <p class="remove" onClick="removeCard(this)">x</p>
     </li>`;
@@ -17,9 +17,30 @@ function addCard(element) {
   board.innerHTML = board.innerHTML + template;
 }
 
+
 function removeCard(element) {
   const li = element.parentElement.id; // pega o id do elemento pai
   console.log('li: ', li)
 
 document.getElementById(li).remove();
+}
+
+
+function drag(event) {
+  event.dataTransfer.setData("card", event.target.id);  // salva o card temporariamente enquando é movido
+}
+
+function over(event) {
+  event.preventDefault();
+}
+
+function drop(event, id) {
+  event.preventDefault();
+  const target = document.getElementById(id);
+  const data = event.dataTransfer.getData("card"); // recupera o card salvo temporariamente
+  const card = document.getElementById(data);
+
+  target.appendChild(card); // adiciona card a nova lista
+
+  event.dataTransfer.clearData(); //limpa o card salvo da memória
 }
